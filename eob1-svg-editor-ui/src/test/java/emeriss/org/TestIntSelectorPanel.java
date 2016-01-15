ba4b1fc1e7545d0e136cc2ab9aed119454104362
Thomas Silvi <thomas.silvi@laposte.net>
2016-01-15 18:27:50 +0100

    ui xp/lvl/food

--- eob1-svg-editor-ui/src/test/java/emeriss/org/TestIntSelectorPanel.java
+++ eob1-svg-editor-ui/src/test/java/emeriss/org/TestIntSelectorPanel.java
@@ -6,30 +6,30 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-public class TestByteSelectorPanel extends TestCase
+public class TestIntSelectorPanel extends TestCase
 {
 
-    public TestByteSelectorPanel( String testName ) {
+    public TestIntSelectorPanel( String testName ) {
         super( testName );
     }
 
     public static Test suite() {
-        return new TestSuite( TestByteSelectorPanel.class );
+        return new TestSuite( TestIntSelectorPanel.class );
     }
 
     public void testInit() {
-        ByteSelectorPanel p = new ByteSelectorPanel(Character.NULL_SCORE, Character.MIN_SCORE, Character.MAX_SCORE);
+        IntSelectorPanel p = new IntSelectorPanel(Character.NULL_SCORE, Character.MIN_SCORE, Character.MAX_SCORE);
         assertTrue( p.getValue() == Character.NULL_SCORE);
     }
 
     public void testSetValue() {
-        ByteSelectorPanel p = new ByteSelectorPanel(Character.NULL_SCORE, Character.MIN_SCORE, Character.MAX_SCORE);
+        IntSelectorPanel p = new IntSelectorPanel(Character.NULL_SCORE, Character.MIN_SCORE, Character.MAX_SCORE);
         p.setValue(Character.MAX_SCORE);
         assertTrue( p.getValue() == Character.MAX_SCORE);
     }
 
     public void testIncrement() {
-        ByteSelectorPanel p = new ByteSelectorPanel(Character.NULL_SCORE, Character.MIN_SCORE, Character.MAX_SCORE);
+        IntSelectorPanel p = new IntSelectorPanel(Character.NULL_SCORE, Character.MIN_SCORE, Character.MAX_SCORE);
         p.setValue(Character.MIN_SCORE);
         for (byte i=0;i<(Character.MAX_SCORE+1);i++) {
             p.increment();
@@ -38,7 +38,7 @@ public class TestByteSelectorPanel extends TestCase
     }
 
     public void testDecrement() {
-        ByteSelectorPanel p = new ByteSelectorPanel(Character.NULL_SCORE, Character.MIN_SCORE, Character.MAX_SCORE);
+        IntSelectorPanel p = new IntSelectorPanel(Character.NULL_SCORE, Character.MIN_SCORE, Character.MAX_SCORE);
         p.setValue(Character.MIN_SCORE);
         for (byte i=0;i<(Character.MAX_SCORE+1);i++) {
             p.decrement();
